72d4d79850c6cfa005691bd6ff53c9518441c3fe
Parthipan Natkunam <natkunamparthipan@gmail.com>
2023-04-08 07:19:28 +0200

    add keyboard interactions and aria-attrs.

--- Modal Dialogs/script.js
+++ Modal Dialogs/script.js
@@ -1,27 +1,67 @@
-
 function toggleOverlay() {
   const overlay = document.getElementById("overlay");
   if (overlay.getAttribute("aria-hidden") === "true") {
     overlay.style.display = "block";
     overlay.setAttribute("aria-hidden", "false");
+    overlay.style.visibility = "visible";
     return;
   }
   overlay.style.display = "none";
   overlay.setAttribute("aria-hidden", "true");
+  overlay.style.visibility = "hidden";
+}
+
+function escapeHandler(e) {
+  const ESCAPE_KEY = 27;
+  if (e.key === "Escape" || e.keyCode === ESCAPE_KEY) {
+    closeModal();
+  }
+}
+
+function focusHandler(e) {
+  const TAB_KEY = 9;
+  const focusableElements = this.querySelectorAll(
+    "a[href], button, textarea, input[type='text'], input[type='radio'], input[type='checkbox'], select"
+  );
+  const firstFocusableElement = focusableElements[0];
+  const lastFocusableElement = focusableElements[focusableElements.length - 1];
+  if (e.key === "Tab" || e.keyCode === TAB_KEY) {
+    if (e.shiftKey) {
+      if (document.activeElement === firstFocusableElement) {
+        lastFocusableElement.focus();
+        e.preventDefault();
+      }
+    } else {
+      if (document.activeElement === lastFocusableElement) {
+        firstFocusableElement.focus();
+        e.preventDefault();
+      }
+    }
+  }
+}
+
+function trapKeyboardFocus(modal) {
+  modal.addEventListener("keydown", focusHandler);
+}
+
+function releaseKeyboardFocus(modal) {
+  modal.removeEventListener("keydown", focusHandler);
 }
 
 function openModal() {
   toggleOverlay();
   const modal = document.getElementById("modal-dialog");
-  modal.setAttribute("aria-hidden", "false");
   modal.style.display = "block";
-  modal.focus();
+  modal.style.visibility = "visible";
+  trapKeyboardFocus(modal);
+  document.addEventListener("keydown", escapeHandler);
 }
 
 function closeModal() {
   toggleOverlay();
   const modal = document.getElementById("modal-dialog");
-  modal.setAttribute("aria-hidden", "true");
-  modal.blur();
+  modal.style.visibility = "hidden";
   modal.style.display = "none";
+  releaseKeyboardFocus(modal);
+  document.removeEventListener("keydown", escapeHandler);
 }
